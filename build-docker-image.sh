6c523d75c96934fdf5d89f438dcc05ddd988bd18
hsh814 <shhan@unist.ac.kr>
2026-06-12 06:12:23 +0000

    Cleanup

--- build-docker-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule sync && git submodule update --init
-cd solver/fuzzy-sat && git fetch && git submodule sync && git submodule update --init
-
-docker build -t fuzzolic:latest -f docker/fuzzolic-runner/Dockerfile.Ubuntu2004 .
